ca252893bdc134d4791fa93d4841b6481d86e2b8
Franklin Lee <flee97@live.com>
2021-03-18 22:19:29 -0400

    Changed on sleep line 7
    Change timer from 1m to 2m

--- chbkg.sh
+++ chbkg.sh
@@ -4,6 +4,6 @@ BACKGROUND="$(ls .backgrounds | shuf -n 1)"
 while :
 do
  feh --image-bg \#01070a --bg-max ".backgrounds/${BACKGROUND}"
- sleep 1m
+ sleep 2m
  BACKGROUND="$(ls .backgrounds | grep -v "${BACKGROUND}" | shuf -n 1)"
 done
